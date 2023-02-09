087d83e29c06c80ec2e2023a6f0fd7860355e51f
chandra <chandrapenugonda655@gmail.com>
2023-02-09 23:10:14 +0530

    install vercel

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./main.js",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
